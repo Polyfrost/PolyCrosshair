20e1ba413ea756e8ac18d4aaaca1ad6fa71a184b
Deftu <me@deftu.dev>
2025-01-08 17:40:26 +0200

    Fix root.gradle.kts

--- root.gradle.kts
+++ root.gradle.kts
@@ -9,6 +9,14 @@ preprocess {
     // "1.12.2-forge"(11202, "srg") {
     //     "1.8.9-forge"(10809, "srg")
     // }
-    "1.8.9-forge"(10809, "srg")
+
+    "1.12.2-fabric"(1_12_02, "yarn") {
+        "1.12.2-forge"(1_12_02, "srg") {
+            "1.8.9-forge"(1_08_09, "srg") {
+                "1.8.9-fabric"(1_08_09, "yarn")
+            }
+        }
+    }
+
     strictExtraMappings.set(true)
 }
